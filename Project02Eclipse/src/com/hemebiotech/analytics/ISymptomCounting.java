6d9d53e65f03435002d2325b6dd7118ac33a4faf
Norana95 <boublenza.n@gmail.com>
2021-07-18 03:00:53 +0100

    calling program's methods

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomCounting.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomCounting.java
@@ -3,8 +3,14 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author Norhene
+ * @date 19/07/2021
+ * 
+ 		* count the number of occurrences of symptoms and put them in a TreeMap with their symptom key
+ 		*/
+
 public interface ISymptomCounting {
-	
 	
 	public Map<String, Integer> counting(List<String> list);
 	
