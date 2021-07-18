6b512cc451a25d220fb42a6dc28b9bb92d3c2c7f
Norana95 <boublenza.n@gmail.com>
2021-07-18 02:37:21 +0100

    creation of class and interface for counting symptoms

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/CountingSymptoms.java
@@ -0,0 +1,36 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author Norhene
+ * @return a TreeMap
+ *
+ */
+public class CountingSymptoms implements ISymptomCounting {
+
+	
+
+	public CountingSymptoms() {	}
+
+	/**
+	 * @return treeMap browse a list of symptoms line by line,counter number of
+	 *         occurrence and add it in a map<string,Integer>
+	 */
+	public Map<String, Integer> counting(List<String> list) {
+
+		Map<String, Integer> map = new TreeMap<String, Integer>();
+
+		for (String i : list) {
+			if (map.containsKey(i)) {
+				map.put(i, map.get(i) + 1);
+			} else {
+				map.put(i, 1);
+			}
+		}
+		return map;
+	}
+
+}
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomCounting.java
@@ -0,0 +1,11 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ISymptomCounting {
+	
+	
+	public Map<String, Integer> counting(List<String> list);
+	
+}
