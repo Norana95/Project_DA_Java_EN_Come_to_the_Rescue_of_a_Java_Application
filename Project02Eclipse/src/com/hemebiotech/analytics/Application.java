f82f1c1ae6027a3e9b374dab5ab4d7ab45f5864c
Norana95 <boublenza.n@gmail.com>
2021-07-18 03:00:53 +0100

    calling program's methods

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Application.java
@@ -0,0 +1,17 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.TreeMap;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		AnalyticsCounter anCounter = new AnalyticsCounter();
+
+		List<String> listSymptoms = anCounter.readerSymptoms();
+		TreeMap<String, Integer> symMap = anCounter.countingSymptoms(listSymptoms);
+		anCounter.WriterSymptoms(symMap);
+
+	}
+}
